e5937ab20a998cf33371b1aba2d4903deab4db2e
Jisa_Mathew <35719226+jisamathew@users.noreply.github.com>
2023-02-27 12:52:46 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,10 @@
-// Allows us to use ES6 in our migrations and tests.
-// require('babel-register')
 const HDWalletProvider = require('truffle-hdwallet-provider');
 require('dotenv').config();
 
 const MNEMONIC = "dolphin worth access eyebrow sunset practice deposit build course response unlock steel"
 const REMOTE_NODE = "https://goerli.infura.io/v3/37d174c0a6274d24bd0b557836e001fb";
+
+const REMOTE_NODE_SEPOLIA = "https://sepolia.infura.io/v3/37d174c0a6274d24bd0b557836e001fb";
 // const REMOTE_NODE='https://goerli.infura.io/v3/' + process.env.INFURA_API_KEY;
 
 module.exports = {
@@ -21,9 +21,24 @@ module.exports = {
         return new HDWalletProvider(MNEMONIC, REMOTE_NODE)
       },
       network_id: '5', // eslint-disable-line camelcase
-      gas: 4465030,
-      gasPrice: 10000000000,
+      // gas: 4465030,
+      // gasPrice: 10000000000,
+      timeoutBlocks: 200,
+      networkCheckTimeout: 999999,  
+      skipDryRun: true 
     },
+    sepolia: {
+      provider: () => 
+      new HDWalletProvider(
+        MNEMONIC, REMOTE_NODE_SEPOLIA      
+      ),
+      network_id: 11155111,
+      gas:5221975,
+      gasPrice:20000000000,
+      confirmations: 3,
+      timeoutBlocks:200,
+      skipDryRun: true
+    }
   },
 
   mocha: {
@@ -35,30 +50,3 @@ module.exports = {
     }
   }
 }
-// // Setup
-// import { Network, Alchemy } from 'alchemy-sdk';
-
-// const settings = {
-//     apiKey: "7xJqA9CGdVqsJzsMiaEJ3fLAZ6sFwc3Y",
-//     network: Network.ETH_GOERLI,
-// };
-
-// const alchemy = new Alchemy(settings);
-
-// // Get the latest block
-// const latestBlock = alchemy.core.getBlockNumber();
-
-// // Get all outbound transfers for a provided address
-// alchemy.core
-//     .getTokenBalances('0x994b342dd87fc825f66e51ffa3ef71ad818b6893')
-//     .then(console.log);
-
-// // Get all the NFTs owned by an address
-// const nfts = alchemy.nft.getNftsForOwner("0xshah.eth");
-
-// // Listen to all new pending transactions
-// alchemy.ws.on(
-//     { method: "alchemy_pendingTransactions",
-//     fromAddress: "0xshah.eth" },
-//     (res) => console.log(res)
-// );
